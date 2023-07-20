03eceb0e857a82cf454c177796b5801931ccc350
Aks <fj-pam@mail.ru>
2023-07-20 21:40:01 +0300

    Отработал 1 Семинар

---
+++ Seminar1/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+int intNumber = 25; //тип данных целочисленные
+double doubleNumber = 12.12; //тип данных десятичные
+string text = "some text";
+bool logicVar = true;
+*/
+
+// int num1 = 5;
+// int num2 = 10;
+//Console.WriteLine ($"My number are {num1} and {num1} and it is good!");
+//Console.WriteLine ("My number are " + num1 + " and " + num1 + " and it is good!");
+
+
+// Console.Write ("input a number ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("My number are " + num1 + " and " + num1 + " and it is good!");
+// Console.WriteLine ($"My number are {num1} and {num1} and it is good!");
+
+
+//Task 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+if(num1 == quad2)
+{ // если команда одна в этих скобках то можно не ставить их
+//if(num1 == quad2) Console.WriteLine("Yes!")
+//else Console.WriteLine("No!");
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+*/
+
+//Task 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+/*
+Console.Write("Input a number ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = -num;
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
