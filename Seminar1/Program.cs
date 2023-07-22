62933c811f9d431c6c0b14df4bf74c910b4007d8
Aks <fj-pam@mail.ru>
2023-07-22 17:37:38 +0300

    Выполнил Задачу 4

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -66,14 +66,36 @@ else {Console.Write(num2);}
 */
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
-
-/*2, 3, 7 -> 7
+/*
+Console.Write("Введите первое число - ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число - ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (max > num2)
+{
+   if (max > num3)
+   {
+    Console.WriteLine(max);
+   }
+}
+max = num2;
+if (max > num3)
+{
+    Console.WriteLine(max);
+}
+else {Console.WriteLine(num3);}
+2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22
+*/
 
-Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-4 -> да
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+
+/*4 -> да
 -3 -> нет
 7 -> нет
 
