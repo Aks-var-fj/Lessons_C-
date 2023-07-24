ec1026c20ee71d3989807f871196496fcc357639
Aks <fj-pam@mail.ru>
2023-07-24 20:58:09 +0300

    Сделал задачу 1 на лекции Seminar2

--- Seminar2_Metody/Program.cs
+++ Seminar2_Metody/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Seminar 2. Методы и функции
+
+// Theory
+/*
+13254 / 10 = 1325 целочисленное деление
+13254 / 100 = 132
+13254 / 1000 = 13
+
+13254 % 10 = 4 поиск остатка от деления
+13254 % 100 = 54
+13254 % 1000 = 254
+
+24365 -> 43 делим на сто, потом извлекаем остаток от деления на сто
+*/
+
+// Практические задания на лекции
+ 
+// Task 1. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+/*
+//351 -> 3 1 -> 31
+int CutNumber(int num)
+{
+    int hundreds = num / 100;
+    int units = num % 10;
+    int res = hundreds * 10 + units; // можно прописать так  (return hundreds * 10 + units;)
+    return res;
+}
+int randNumber = new Random().Next(100,1000);
+int newNumber = CutNumber(randNumber);
+Console.WriteLine($"New version of {randNumber} is {newNumber}");
+*/
+
+// Task 2. 
+
