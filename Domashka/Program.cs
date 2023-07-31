452d769a1401003d234a6d498e172c3c7da259e5
Aks <fj-pam@mail.ru>
2023-07-31 20:56:47 +0300

    Выполнил домашнее задание по семинару 2

--- Domashka/Program.cs
+++ Domashka/Program.cs
@@ -59,6 +59,34 @@ else Console.WriteLine("Нет");
 
 //                                                                            Seminar 3
 
+// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
 
 
+// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Vozvedenie_V_Kub(int number)
+    {
+        if (number <= 0) Console.WriteLine("Uncorrectected number");
+        else
+            {
+                int count = 1;
+                while (count <= number)
+                {
+                    Console.WriteLine($"{count} - {Math.Pow(5,2)}");
+                    count++;
+                }
+            }
+    }
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+void(num);
 
