35e2fdd3fe186a04042b4fd5eaa82de5f5e951e9
Aks <fj-pam@mail.ru>
2023-08-03 02:50:59 +0300

    Выполнил домашнюю работу к семинару 3, задачи №№ 19,21,23

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -61,7 +61,7 @@ Squaer(num);
 */
 
 //Task 4. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
-/*
+
 double Distance(double x1, double y1, double x2, double y2)
 
 {
@@ -78,7 +78,7 @@ double x2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Input y2: ");
 double y2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine(Distance (x1, y1, x2, y2));
-*/
+
 
 
 
