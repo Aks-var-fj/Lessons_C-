165bb90d85a02e77b6cfeb0faa9f7572616e2a1d
Aks <fj-pam@mail.ru>
2023-08-05 01:21:22 +0300

    Ydalil .gitignore

--- Seminar_4_Homework/Program.cs
+++ Seminar_4_Homework/Program.cs
@@ -24,12 +24,13 @@ int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(sum(number));
 */
 //Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
-int[] CreateRandomArray(int size, int minValue, int maxValue)
+int[] CreateRandomArray(int size)
 {
     int[] array = new int[size];
 
     for(int i = 0; i < size; i++)
-        array[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"Input number in {i} element in array: ");
+        array[i] = new Convert.ToInt32(Console.ReadLine());
     return array;
 }
 
@@ -42,13 +43,11 @@ void WriteArray(int[] array)
 
 Console.Write("Input a length of an array: ");
 int length = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a min possible value: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a max possible value: ");
-int max = Convert.ToInt32(Console.ReadLine());
 
-int[] myArray = CreateRandomArray(length, min, max);
+int[] myArray = CreateRandomArray(length);
 WriteArray(myArray);
+
+
 
 
 
