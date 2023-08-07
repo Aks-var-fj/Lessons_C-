e3bc3d5b852fb6063514c73f72816d2aeae7f13c
Aks <fj-pam@mail.ru>
2023-08-07 16:02:46 +0300

    Рещил ДЗ по Семинару 5

--- Seminar_5_Homework/Program.cs
+++ Seminar_5_Homework/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//Напишите программу, которая покажет количество чётных чисел в массиве.
 /*
 int[] create_random_array (int size) // можно ли вместо аргументов оставить поле пустым а в цикле сделать рандомнекст?
 {
@@ -28,7 +29,9 @@ int[] my_array = create_random_array(length);
 write_array(my_array);
 Console.WriteLine(number_of_even_digits(my_array));
 */
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+//Найдите сумму элементов, стоящих на нечётных позициях.
 /*
 int[] create_random_array (int size, int min_value, int max_value)
 {
@@ -62,12 +65,49 @@ int [] my_array = create_random_array(length, min_value, max_value);
 write_array(my_array);
 Console.Write(sum_of_negatives(my_array));
 */
-// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и
+//минимальным элементов массива.
+/*
+double[] create_random_array (int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Enter number in array with index {i}: ");
+            array[i] = Convert.ToDouble(Console.ReadLine());
+        }
+    return array;
+}
+void write_array (double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+double difference_between_max_min (double[] array)
+{
+    double result = 0;
+    double min = array[array.Length - 2];
+    double max = array[array.Length - 1];
+    for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > max)
+                max = array[i];
+            if (array[i] < min)
+                min = array[i];
+            result = max - min;
+        }
+    return result;
+}
 
+Console.Write("Enter a length of an array: ");
+int length = Convert.ToInt32(Console.ReadLine());
 
-
+double[] my_array = create_random_array(length);
+write_array(my_array);
+Console.WriteLine(difference_between_max_min(my_array));
+*/
 
 
 
