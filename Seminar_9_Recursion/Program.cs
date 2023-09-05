e006f4faf8d6dec4842625d7e5d9a0428df8660b
Aks <fj-pam@mail.ru>
2023-09-05 12:42:12 +0300

    Сделал задачу 63  9 семинара

--- Seminar_9_Recursion/Program.cs
+++ Seminar_9_Recursion/Program.cs
@@ -1,26 +1,66 @@
-﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+﻿//Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+/*
+// void print_number (int num)
+// {
+//     int res = 0;
+//     while (res < num)
+//     {
+//         res++;
+//         System.Console.Write(res);
+//     }
+// }
 
-void print_number (int num)
+// System.Console.WriteLine("Input the number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// print_number(num);
+
+// string PrintNumer(int num, string res)
+// {
+// if (num == 1)
+// {
+// res = res + '1';
+// return res;
+// }
+// else
+// {
+// res = res + num;
+// return PrintNumer(num - 1, res);
+// }
+// }
+// string result = " ";
+
+// Console.Write("Input the number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// result = PrintNumer(number, result);
+// Console.WriteLine(result);
+
+
+string PrintNumbers(int start, int end)
 {
-    int res = 0;
-    while (res < num)
-    {
-        res++;
-        System.Console.Write($"N = {res} ");
-    }
+    if (start == end) return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
 }
 
-System.Console.WriteLine("Input the number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-print_number(num);
-
-
-
-
-
-//Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
-// N = 5 -> "1, 2, 3, 4, 5"
-// N = 6 -> "1, 2, 3, 4, 5, 6"
+Console.WriteLine(PrintNumbers(1,8));
+*/
 // Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 6, 7, 8"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
